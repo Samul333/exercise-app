c65708c02d9a400cc3a5a2ac0b59a384feaef16e
Samul <samulshrestha101@gmail.com>
2020-08-01 21:26:31 +0545

    Updates

---
+++ precache-manifest.968990474a430d8914b507aeca8f44c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93550357c747d72eb8f2cf1bb8aad6dd",
+    "url": "/exercise-app/index.html"
+  },
+  {
+    "revision": "c415c38264134fc22cd6",
+    "url": "/exercise-app/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "a1b21de2a5ca369c3028",
+    "url": "/exercise-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c415c38264134fc22cd6",
+    "url": "/exercise-app/static/js/2.8401eef0.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exercise-app/static/js/2.8401eef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b21de2a5ca369c3028",
+    "url": "/exercise-app/static/js/main.7aa20f28.chunk.js"
+  },
+  {
+    "revision": "1df05747499882f6fb00",
+    "url": "/exercise-app/static/js/runtime-main.c948032a.js"
+  }
+]);
